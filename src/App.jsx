7edb91db2b5712e75e00d4c757583e1de1b45981
Saranya <saranya@heptotechnologies.org>
2026-07-24 14:37:00 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,13 @@ import Layout from './components/Layout'
 import HomePage from './pages/HomePage'
 import CategoryPage from './pages/CategoryPage'
 import ProductDetails from './pages/ProductDetails'
+// import TryAtHomePage from './pages/TryAtHomePage'
+// import VideoCallPage from './pages/VideoCallPage'
+// import DigitalGoldBanner from './components/DigitalGoldBanner'
+// import DigitalGoldPage from './pages/DigitalGoldPage'
+import SignInPage from './pages/SignInPage'
+import WishlistPage from './pages/WishlistPage'
+import CartPage from './pages/CartPage'
 
 function App() {
   return (
@@ -11,7 +18,10 @@ function App() {
         <Route index element={<HomePage />} />
         <Route element={<CategoryPage />} path="category/:slug" />
         <Route element={<ProductDetails />} path="product/:productId" />
+        <Route element={<SignInPage />} path="/sign-in" />
+        <Route element={<WishlistPage />} path="/wishlist" />
       </Route>
+      <Route element={<CartPage />} path="/bag" />
     </Routes>
   )
 }
